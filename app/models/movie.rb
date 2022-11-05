07a544426827046f84753d786245bdfd5306ad17
Ronaldkubasu <ronald.kubasu@student.moringaschool.com>
2022-11-05 16:38:20 +0300

    completed assignment

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,51 @@
 class Movie < ActiveRecord::Base
+    def in_theaters?
+        self.in_theaters
+    end
 
+    def self.create_with_title(title)
+      # movie = Movie.new(title:title)
+      Movie.create(title:title)
+    end
+
+    def self.first_movie
+      Movie.first
+    end
+
+    def self.last_movie
+      Movie.last
+    end
+
+    def self.movie_count
+      Movie.all.count
+    end
+
+    def self.find_movie_with_id(id)
+      Movie.find(id)
+    end
+
+    def self.find_movie_with_attributes(t)
+      Movie.find_by(t)
+    end
+
+    def self.find_movies_after_2002
+      Movie.where("release_date>2002")
+    end
+
+    def update_with_attributes(title:)
+      self.update(title:title)
+    end
+
+    def self.update_all_titles(title)
+      Movie.update_all(title: title)
+    end
+
+    def self.delete_by_id(id)
+      Movie.destroy_by(id: id)
+    end
+
+    def self.delete_all_movies
+      Movie.delete_all
+    end
+    
 end
